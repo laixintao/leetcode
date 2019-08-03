666aaa2e7a9fc3812a74669f3d591cc0d0854133
laixintao <laixintaoo@gmail.com>
2019-08-04 00:00:00 +0800

    0041

---
+++ main.py
@@ -0,0 +1,64 @@
+from typing import List
+from collections import Counter, defaultdict
+import bisect
+import itertools
+
+
+def create_tree(values):
+    root = TreeNode(values.pop(0))
+    roots = [root]
+    while values:
+        left, right = values.pop(0), values.pop(0)
+        node = roots.pop(0)
+        node.left = TreeNode(left)
+        node.right = TreeNode(right)
+        roots.extend([node.left, node.right])
+    return root
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return f"[{self.val}]({self.left}, {self.right})"
+
+    def __repr__(self):
+        return f"TreeNode({self.val})"
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+case = 0
+
+
+def test(*args):
+    global case
+    print(f"---case {case}---")
+    case += 1
+    print(f"{args=}")
+    s = Solution()
+    ans = s.readBinaryWatch(*args)
+    print("ans=", ans)
+
+
+class Solution:
+    def readBinaryWatch(self, num: int) -> List[str]:
+        return [
+            "%d:%0.2d" % (h, m)
+            for h in range(12)
+            for m in range(60)
+            if (bin(h) + bin(m)).count("1") == num
+        ]
+
+
+if __name__ == "__main__":
+    test(1)
