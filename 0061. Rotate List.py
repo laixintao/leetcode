dd5b7f54b00f2fb5883a448bf0ccfa476f186e4a
laixintao <laixintaoo@gmail.com>
2020-01-30 00:00:00 +0800

    61

--- 0061. Rotate List.py
+++ 0061. Rotate List.py
@@ -1,1 +1,27 @@
-class Solution:    def readBinaryWatch(self, num: int) -> List[str]:        
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if not head:
+            return None
+        if head.next is None:
+            return head
+        total = 1
+
+        tail = head
+        while tail.next:
+            total += 1
+            tail = tail.next
+
+        if k % total == 0:
+            return head
+
+        rotate = total - k % total - 1
+
+        counter = head
+        while rotate:
+            counter = counter.next
+            rotate -= 1
+
+        new_head = counter.next
+        counter.next = None
+        tail.next = head
+        return new_head
